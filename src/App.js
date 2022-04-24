241ca5d49ede3a2bf6479ef04e0c189524ba70a4
sultonbek <zhoroevsultonbek@gmail.com>
2022-04-24 06:39:04 +0300

    added class component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import House from "./pages/House";
 import About from "./pages/About";
 import News from "./pages/News";
 import ErrorPage from "./pages/ErrorPage";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<House />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/news" element={<News />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<House />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/news" element={<News />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
